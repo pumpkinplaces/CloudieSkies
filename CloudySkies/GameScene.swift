8d988abd8a38b0a73a13961c909067542b10dd85
Amanda Viava <vialvaamanda@gmail.com>
2020-07-26 18:17:47 -0400

    - commit

--- CloudySkies/GameScene.swift
+++ CloudySkies/GameScene.swift
@@ -9,144 +9,92 @@
 import SpriteKit
 import GameplayKit
 
-
+class AllBunnyInfo{
+    static var ownsList:[String:Bool] = [:]
+}
 
 class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    
-    var Bunny = SKSpriteNode()
-    var Cloud = SKSpriteNode()
-    //var background1 = SKSpriteNode()
-    var background2 = SKSpriteNode()
-    var startScreenDone = false
-     
-    
-    override func didMove(to view: SKView) {
+    var image = SKSpriteNode()
         
-        // Get label node from scene and store it for use later
-        //self.backgroundColor = UIColor.brown
-    
-        //print(self.frame.width, self.frame.height, separator: "  ")
-        setUpUI()
-        self.addChild(Bunny)
-        self.addChild(label!)
-        self.addChild(background2)
-        self.addChild(Cloud)
+    override func didMove(to view: SKView) {
+    // Get label node from scene and store it for use later
+       // self.backgroundColor = UIColor(red: 128, green: 75, blue: 71)
+        firstLaunchSettings()
+        self.backgroundColor = UIColor.black
+        image = SKSpriteNode(imageNamed: "AirDares")
+        image.position = CGPoint(x: 0, y: 0)
+        image.setScale(1.22)
+        self.addChild(image)
+        image.zPosition = 1
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            let intro = IntroScene(fileNamed: "PlayScene")
+            let fadeAway = SKTransition.fade(with: UIColor.systemTeal, duration: 1)
+            self.scene?.view?.presentScene(intro!, transition: fadeAway)
+        })
     }
-    
-   
-    private func slideOff(node: SKNode, float: CGFloat) -> SKAction {
-        return SKAction.run() {
-            var yPos = node.position.y
-           
-            while yPos >= -self.frame.height/2 - node.frame.height {
-                let move = SKAction.moveTo(y: yPos - float, duration: 1.24)
-                node.run(move)
-                yPos -= float
+        
+    private func firstLaunchSettings(){
+        if UserDefaults().bool(forKey: "First Launch") == true{
+            UserDefaults.standard.set(true, forKey: "SoundOffOrOn")
+            UserDefaults.standard.set("Bunny", forKey: "bunnyType")
+            UserDefaults.standard.set("", forKey: "RabbitaColor")
+            UserDefaults.standard.set(0, forKey: "CarrotCount")
+            UserDefaults.standard.set("StoreScene", forKey: "StoreScene")
+            UserDefaults.standard.set(0, forKey: "highscore")
+            UserDefaults.standard.set(0, forKey: "highestTotalScore")
+            AllBunnyInfo.ownsList["Bunny"] = true; AllBunnyInfo.ownsList["YellowRabbita"] = false; AllBunnyInfo.ownsList["BrownRabbit"] = false; AllBunnyInfo.ownsList["BeigeBunny"] = true; AllBunnyInfo.ownsList["Shiny"] = false; AllBunnyInfo.ownsList["LightBrownBunny"] = false; AllBunnyInfo.ownsList["DarkBrownBunny"] = false; AllBunnyInfo.ownsList["GreenRabbita"] = false; AllBunnyInfo.ownsList["BlueRabbita"] = false; AllBunnyInfo.ownsList["GrayRabbita"] = false; AllBunnyInfo.ownsList["PinkRabbita"] = false; AllBunnyInfo.ownsList["Rabbita"] = false; AllBunnyInfo.ownsList["OrangeRabbita"] = false; AllBunnyInfo.ownsList["RedRabbita"] = false; AllBunnyInfo.ownsList["PurpleRabbita"] = false
+            UserDefaults.standard.set(AllBunnyInfo.ownsList, forKey: "Owned")
+        }
+    }
+        func touchDown(atPoint pos : CGPoint) {
+            if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+                n.position = pos
+                n.strokeColor = SKColor.green
+                self.addChild(n)
             }
         }
+        
+        
+        func touchMoved(toPoint pos : CGPoint) {
+            if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+                n.position = pos
+                n.strokeColor = SKColor.blue
+                self.addChild(n)
+            }
+        }
+        
+
+        func touchUp(atPoint pos : CGPoint) {
+            if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+                n.position = pos
+                n.strokeColor = SKColor.red
+                self.addChild(n)
+            }
+        }
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
-    
-    
-    private func setUpUI(){
-        Bunny = SKSpriteNode(imageNamed: "Bunny1")
-        Bunny.setScale(0.75)
-        Bunny.position = CGPoint(x: 0, y: 175)
-        Bunny.zPosition = 4
         
-        label = SKLabelNode(text: "Cloudy Skies")
-        label!.setScale(1)
-        label!.position = CGPoint(x: 0, y: -400)
-        label!.fontSize = 100
-        label!.fontColor = UIColor.systemBlue
-        label!.fontName = "Noteworthy-Bold"
-        label!.alpha = 0
-        label!.zPosition = 1
+        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        }
         
-        /*
-        background1 = SKSpriteNode(imageNamed: "Background1")
-        background1.position = CGPoint(x: 0, y: 0)
-        background1.setScale(1)
-        background1.zPosition = 0
-        */
+        override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        }
         
-        background2 = SKSpriteNode(imageNamed: "Background2")
-        background2.position = CGPoint(x: 130, y: 0)
-        background2.setScale(1)
-        background2.zPosition = 0
-               
-       /*
-        let fadeOut = SKAction.fadeOut(withDuration:5)
-        let falloffScene = SKAction.falloff(to: -300, duration: 10)
-        let labelAsNode: SKNode = label!
-        */
-        let waitForIt = SKAction.wait(forDuration: 0.05)
-        let moveDown = slideOff(node: label!, float: 10)
-        let itllFade = SKAction.fadeIn(withDuration: 1.5)
-        let sequence = SKAction.sequence([itllFade, waitForIt, moveDown])
-        label!.run(sequence)
+        override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        }
         
-        //let moveOff = SKAction.falloff(by: 500, duration: 2)
-        //label!.run(SKAction.sequence([moveIn, moveOff]))
-        //label!.run(SKAction.sequence([moveIn]))
-        Cloud = SKSpriteNode(imageNamed: "Cloud")
-        Cloud.position = CGPoint(x: 0, y: -111)
-        Cloud.zPosition = 1
-        Cloud.setScale(0.5)
-    }
-    
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+        
+        override func update(_ currentTime: TimeInterval) {
+            // Called before each frame is rendered
         }
     }
-    
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-}
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        if label!.hasActions() == false {
-        let mainGameScene = MainGame(fileNamed: "MainGame")
-            let fadeAway = SKTransition.fade(with: UIColor.systemTeal, duration: 1)
-            self.scene?.view?.presentScene(mainGameScene!, transition: fadeAway)
-        }
-    }
-}
+
+
+  
--- CloudySkies/IntroScene.swift
+++ CloudySkies/IntroScene.swift
@@ -1,3 +1,5 @@
+
+
 //
 //  GameScene.swift
 //  CloudySkies
@@ -13,70 +15,121 @@ import GameplayKit
 
 class IntroScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    var image = SKSpriteNode()
-
-    
-    override func didMove(to view: SKView) {
+        private var label : SKLabelNode?
+        private var spinnyNode : SKShapeNode?
         
-    // Get label node from scene and store it for use later
-        self.backgroundColor = UIColor(red: 128, green: 75, blue: 71)
-        image = SKSpriteNode(imageNamed: "AirDares")
-        image.position = CGPoint(x: 100, y: 0)
-        image.setScale(0.45)
-        self.addChild(image)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
-            let gamescene = GameScene(fileNamed: "GameScene")
-            let fadeAway = SKTransition.fade(with: UIColor.systemTeal, duration: 1)
-            self.scene?.view?.presentScene(gamescene!, transition: fadeAway)
-        })
-       
-    }
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
+        var Bunny = SKSpriteNode()
+        var Cloud = SKSpriteNode()
+        var background2 = SKSpriteNode()
+        var startScreenDone = false
 
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+        
+        override func didMove(to view: SKView) {
+            // Get label node from scene and store it for use later
+            setUpUI()
+            self.addChild(Bunny)
+            self.addChild(label!)
+            self.addChild(background2)
+            self.addChild(Cloud)
+        }
+        
+        private func slideOff(node: SKNode, float: CGFloat) -> SKAction {
+            return SKAction.run() {
+                var yPos = node.position.y
+                while yPos >= -self.frame.height/2 - node.frame.height {
+                    let move = SKAction.moveTo(y: yPos - float, duration: 1.24)
+                    node.run(move)
+                    yPos -= float
+                }
+            }
+        }
+        
+        
+        private func setUpUI(){
+            Bunny = SKSpriteNode(imageNamed: "Bunny1")
+            Bunny.setScale(0.485)
+            Bunny.position = CGPoint(x: 0, y: 110)
+            Bunny.zPosition = 4
+            
+            label = SKLabelNode(text: "Cloudy Skies")
+            label!.setScale(0.67)
+            label!.position = CGPoint(x: 0, y: -250)
+            label!.fontSize = 100
+            label!.fontColor = UIColor.systemBlue
+            label!.fontName = "Noteworthy-Bold"
+            label!.alpha = 0
+            label!.zPosition = 2
+        
+            background2 = SKSpriteNode(imageNamed: "Background2")
+            background2.position = CGPoint(x: 130, y: 0)
+            background2.setScale(1)
+            background2.zPosition = 0
+            
+            let waitForIt = SKAction.wait(forDuration: 0.05)
+            let itllFade = SKAction.fadeIn(withDuration: 1.5)
+            
+            let moveDown3 = SKAction.moveTo(y: -self.frame.height/2 - 200, duration: 1.7)
+            let sequ2 =  SKAction.sequence([itllFade, waitForIt, moveDown3])
+            label!.run(sequ2)
+
+            Cloud = SKSpriteNode(imageNamed: "Cloud")
+            Cloud.position = CGPoint(x: 0, y: -65)
+            Cloud.zPosition = 1
+            Cloud.setScale(0.32)
+        }
+       
+        
+        func touchDown(atPoint pos : CGPoint) {
+            if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+                n.position = pos
+                n.strokeColor = SKColor.green
+                self.addChild(n)
+            }
+        }
+        
+        
+        func touchMoved(toPoint pos : CGPoint) {
+            if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+                n.position = pos
+                n.strokeColor = SKColor.blue
+                self.addChild(n)
+            }
+        }
+        
+
+        func touchUp(atPoint pos : CGPoint) {
+            if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+                n.position = pos
+                n.strokeColor = SKColor.red
+                self.addChild(n)
+            }
+        }
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+        
+        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        }
+        
+        override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        }
+        
+        override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        }
+        
+        
+        override func update(_ currentTime: TimeInterval) {
+            // Called before each frame is rendered
+            if label!.hasActions() == false {
+                let introscene = PlayScene(fileNamed: "PlayScene")
+                let fadeAway = SKTransition.fade(with: UIColor.systemTeal, duration: 1)
+                self.scene?.view?.presentScene(introscene!, transition: fadeAway)
+            
+            }
         }
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-}
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
+
+
