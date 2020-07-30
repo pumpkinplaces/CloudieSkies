8951f42e106090c597449466f1cd480608838421
Amanda Viava <vialvaamanda@gmail.com>
2020-07-30 02:37:05 -0400

    -

--- CloudySkies/GameViewController.swift
+++ CloudySkies/GameViewController.swift
@@ -11,13 +11,17 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+     var mainGameScene = GameScene()
+    
+//    
     override func viewDidLoad() {
-        super.viewDidLoad()
         
+        super.viewDidLoad()
+
+        //createGradientLayer()
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = SKScene(fileNamed: "GameScene")  {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
@@ -26,12 +30,13 @@ class GameViewController: UIViewController {
             }
             
             view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsFPS = false
+            view.showsNodeCount = false
         }
     }
 
+    
+    
     override var shouldAutorotate: Bool {
         return true
     }
