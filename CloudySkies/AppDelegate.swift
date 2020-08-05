e3189a4dc6d152226cbfce225955cab44c7b9948
Amanda Viava <vialvaamanda@gmail.com>
2020-08-05 07:44:02 -0400

    --

--- CloudySkies/AppDelegate.swift
+++ CloudySkies/AppDelegate.swift
@@ -36,5 +36,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        if UserDefaults().bool(forKey: "hasLaunched") != true{
+            UserDefaults.standard.set(false, forKey: "hasLaunched")
+        }
+    }
 }
 
