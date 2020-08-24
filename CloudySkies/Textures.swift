bf079e082b0976f303600f320807938d096e9ac8
Amanda Viava <vialvaamanda@gmail.com>
2020-08-24 09:02:52 -0400

    -

---
+++ CloudySkies/Textures.swift
@@ -0,0 +1,51 @@
+//
+//  Textures.swift
+//  CloudySkies
+//
+//  Created by Amanda Vialva on 8/22/20.
+//  Copyright © 2020 Amanda Vialva. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+class BunnyTexts{
+    static var Cloud1 = SKTexture(imageNamed: "cloudfirst")
+       static var Cloud2 = SKTexture(imageNamed: "cloudsec")
+       static var Cloud3 = SKTexture(imageNamed: "cloudthird")
+       static var Cloud4 = SKTexture(imageNamed: "cloudfourth")
+       static var Bird = SKTexture(imageNamed: "Bird1")
+       static var AirPlane = SKTexture(imageNamed: "AirPlane4")
+       static var RainDrop = SKTexture(imageNamed: "RainDrop")
+       static var Lightening = SKTexture(imageNamed: "Lightening")
+       static var BlueSky = SKTexture(imageNamed: "BlueSky")
+       static var BlueGrayish = SKTexture(imageNamed: "BlueGrayish")
+       static var PurpleSky = SKTexture(imageNamed: "PurpleSky")
+       static var RedOrangeYellow = SKTexture(imageNamed: "RedOrangeYellow")
+       static var BluishBlack = SKTexture(imageNamed: "BluishBlack")
+       static var TheOriginal = SKTexture(imageNamed: "TheOriginal")
+       static var PinkBlue = SKTexture(imageNamed: "PinkBlue")
+       static var BrightBlueSky = SKTexture(imageNamed: "BrightBlueSky")
+       static var DarkRedOrangeYellow = SKTexture(imageNamed: "DarkRedOrangeYellow")
+       static var BluishBrown = SKTexture(imageNamed: "BluishBrown")
+       static var PurpleBlack = SKTexture(imageNamed: "PurpleBlack")
+       static var NorthernLights = SKTexture(imageNamed: "NorthernLights")
+       static var LightAndDarkBlue = SKTexture(imageNamed: "LightAndDarkBlue")
+       static var BrightBluishBrown = SKTexture(imageNamed: "BrightBluishBrown")
+       static var Rainbow = SKTexture(imageNamed: "Rainbow")
+       static var PinkAndGreen = SKTexture(imageNamed: "PinkAndGreen")
+       static var BlueGreen = SKTexture(imageNamed: "BlueGreen")
+       static var Midnight = SKTexture(imageNamed: "Midnight")
+       
+       static var BirdPoo = SKTexture(imageNamed: "Poop")
+       static var PlayAgainBar = SKTexture(imageNamed: "PlayAgainBar")
+       static var MainMenu = SKTexture(imageNamed: "MainMenu")
+       static var PlayButton = SKTexture(imageNamed: "PlayButton")
+       static var SoundOffButton = SKTexture(imageNamed: "SoundOffButton")
+       static var SoundButton = SKTexture(imageNamed: "SoundButton")
+       static var PauseButton = SKTexture(imageNamed: "PauseButton")
+       static var Carrot = SKTexture(imageNamed: "Carrot")
+       
+       static var picList:[SKTexture] = []
+}
