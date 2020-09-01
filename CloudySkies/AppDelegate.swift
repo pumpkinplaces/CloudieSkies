42d73d549ace009491099b1f659f6b21e3c023a1
Amanda Viava <vialvaamanda@gmail.com>
2020-09-01 00:47:59 -0400

    -

--- CloudySkies/AppDelegate.swift
+++ CloudySkies/AppDelegate.swift
@@ -7,22 +7,57 @@
 //
 
 import UIKit
+import Firebase
+import GoogleMobileAds
+import FirebaseMessaging
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+        } else {
+          let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+        Messaging.messaging().delegate = self
+        FirebaseApp.configure()
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
     }
+    
+   
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        InstanceID.instanceID().instanceID { (result, error) in
+            if error != nil {
+                return
+            }
+            else if result != nil {
+                return
+            }
+        }
+    }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
+    
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
@@ -36,6 +71,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
+    
     func applicationDidFinishLaunching(_ application: UIApplication) {
         if UserDefaults().bool(forKey: "hasLaunched") != true{
             UserDefaults.standard.set(false, forKey: "hasLaunched")
@@ -45,6 +81,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         if UserDefaults().bool(forKey: "SecondUpdate") != true{
             UserDefaults.standard.set(false, forKey: "SecondUpdate")
+        }
+        if UserDefaults().bool(forKey: "ThirdUpdate") != true{
+            UserDefaults.standard.set(false, forKey: "ThirdUpdate")
         }
     }
 }
