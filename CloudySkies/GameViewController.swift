85ff4c96ca0914f1f28124b6c7e2f56f46b56dec
Amanda Viava <vialvaamanda@gmail.com>
2020-09-02 16:27:53 -0400

    -

--- CloudySkies/GameViewController.swift
+++ CloudySkies/GameViewController.swift
@@ -94,17 +94,17 @@ class GameViewController: UIViewController, GKGameCenterControllerDelegate, UINa
     
     
      func addBannerViewToView(_ bannerView: GADBannerView) {
-     bannerView.translatesAutoresizingMaskIntoConstraints = false
-     view.addSubview(bannerView)
-     view.addConstraints(
-       [NSLayoutConstraint(item: bannerView,
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
                            attribute: .bottom,
                            relatedBy: .equal,
                            toItem: view.safeAreaLayoutGuide,
                            attribute: .bottom,
                            multiplier: 1,
                            constant: 0),
-        NSLayoutConstraint(item: bannerView,
+             NSLayoutConstraint(item: bannerView,
                            attribute: .centerX,
                            relatedBy: .equal,
                            toItem: view,
